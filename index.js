19da1c1843e9b2bd75c475a053c4567858670f14
Jennifer Rhees <redswinglinestapler23@gmail.com>
2017-11-07 11:20:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(block) {
+  return Math.abs(block - 42);
+}
+
+function distanceFromHqInFeet(block) {
+  return distanceFromHqInBlocks(block) * 264;
+}
